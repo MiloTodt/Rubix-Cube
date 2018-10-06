53b12c669bad43d562711995c2ac30b66b7f1ccf
milotodt <milo@milotodt.com>
2018-10-06 02:32:10 -0700

    starting cube struct

--- src/main.rs
+++ src/main.rs
@@ -7,34 +7,38 @@ fn main()
     let  side2 = Side { faces: vec!['B','B','B','B','B','B','B','B','W']};
     println!("{}", side2.is_solved());
 
-    side2.print_side();
+    println!("{}", side2.print_side());
 
 } 
 
 
-struct Side {
+struct Side { //Holds one side of the cube
     faces: Vec<char>,
 }
 
+struct Cube { //has 6 sides
+    faces: Vec<Side>,
+}
+
 impl Side {
-    fn is_solved(&self) -> bool {
+    fn is_solved(&self) -> bool { //Side is solved if all colours match
         let first = &self.faces[0];
         for face in &self.faces {
             if first != face {return false;}
         }
         true
     }
-        fn print_side(&self) -> () {
-        println!(
+    fn print_side(&self) -> String {
+        format!(
             "
             {}|{}|{}\n
             {}|{}|{}\n
-            {}|{}|{}", self.faces[0], self.faces[1],self.faces[2],self.faces[3],self.faces[4],self.faces[5],self.faces[6],self.faces[7],self.faces[8]
+            {}|{}|{}", self.faces[0], self.faces[1], self.faces[2], self.faces[3], self.faces[4], self.faces[5], self.faces[6], self.faces[7], self.faces[8]
         )
     }
 }
 
-fn build_side(colour: char) -> Side {
+fn build_side(colour: char) -> Side { //Builds a side of all the same colour
     Side{
         faces: vec![colour; 9],
     }
